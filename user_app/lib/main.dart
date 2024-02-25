24f4a56ce3fd8d0ad61eb8069d5db2a97ecbace0
Rajat <85779110+rajatDeveloper@users.noreply.github.com>
2024-02-25 10:53:33 +0530

    Update main.dart

--- user_app/lib/main.dart
+++ user_app/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        
         ChangeNotifierProvider(create: (_) => MainProvider()),
       ],
       child: MaterialApp(
